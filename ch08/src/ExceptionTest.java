5a96b0b6a5ec99fe27c5a25d5660f2dada208074
진성민 <sengmin14@naver.com>
2024-05-09 09:34:00 +0900

    feat : 예외처리 ch08 파일 생성

---
+++ ch08/src/ExceptionTest.java
@@ -0,0 +1,11 @@
+
+public class ExceptionTest {
+
+	// 컴파일 에러 : 컴파일 할 때 발생하는 에러
+	// 런타임 에러 : 실행 할 때 발생하는 에러
+	// 논리적 에러 : 작성 의도와 다르게 동작
+	public static void main(String[] args)
+	{
+		System.out.println(args[0]);
+	}
+}
