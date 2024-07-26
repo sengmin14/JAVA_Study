c564f6e86e9ed7e64d15bd2f67df608450f8db66
진성민 <sengmin14@naver.com>
2024-07-26 20:27:50 +0900

    객체지향1

---
+++ ch06/src/Ex6_11.java
@@ -0,0 +1,22 @@
+class Data_1 {
+	int value;
+}
+
+class Data_2 {
+	int value;
+	
+	Data_2() {
+		
+	}
+	
+	Data_2(int x) {   // �Ű������� �ִ� ������.
+		value = x;
+	}
+}
+
+class Ex6_11 {
+	public static void main(String[] args) {
+		Data_1 d1 = new Data_1();
+		Data_2 d2 = new Data_2(); // compile error�߻�
+	}
+}
