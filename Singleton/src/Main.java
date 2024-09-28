5409b3058c45d2eca93cf318e8be7028bf8e169a
진성민 <sengmin14@naver.com>
2024-09-28 22:17:08 +0900

    싱글턴 학습

--- Singleton/src/Main.java
+++ Singleton/src/Main.java
@@ -3,6 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		Singleton singleton = Singleton.getInstance();
+		Singleton singleton2 = Singleton.getInstance();
+		
+		System.out.println(singleton);
+		System.out.println(singleton2);
+		
 		singleton.showMessage();
 	}
 	
--- Singleton/src/Singleton.java
+++ Singleton/src/Singleton.java
@@ -8,7 +8,7 @@ public class Singleton {
 		
 	}
 	
-	public static Singleton getInstance() {
+	public static synchronized Singleton getInstance() {
 		if(instance == null) {
 			instance = new Singleton();
 		}
